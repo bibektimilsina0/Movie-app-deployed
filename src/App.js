c273ab6cd2e0d88e6a8268bc1890fca4afea3676
bikkibro <bibektimilsina7857@gmail.com>
2023-12-09 23:49:18 +0545

    first

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          BIbek timilsina
         </p>
         <a
           className="App-link"
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+        deploy practice
         </a>
       </header>
     </div>
